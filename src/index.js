79448c2d14d260df93150a80aa613d7937520985
Mbalie3737 <mbaliletsoara31@gmail.com>
2024-02-05 07:38:22 +0200

    added apiUrl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-function searchCity(city) {
+function weatherSearchCity(city) {
     let apiKey = "b3afab731284f4b16ot5f2d9945e5053";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     console.log(apiUrl);
 }
+
 
 
 function searchButton(event) {
@@ -10,7 +11,7 @@ function searchButton(event) {
     let searchInputElement = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-city");
     cityElement.innerHTML = searchInputElement.value;
-    searchCity(searchInput.value);
+    weatherSearchCity(searchInputElement.value);
 
 }
 
