e9029c1fd0e80426590d04666095ad1e6644c0f2
Mbalie3737 <mbaliletsoara31@gmail.com>
2024-02-05 13:31:27 +0200

    added searchWeather using API

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+function weatherSearchUpdate(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+
+}
+
+
 function weatherSearchCity(city) {
     let apiKey = "b3afab731284f4b16ot5f2d9945e5053";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    console.log(apiUrl);
+    axios.get(apiUrl).then(weatherSearchUpdate);
 }
 
 
@@ -9,8 +19,7 @@ function weatherSearchCity(city) {
 function searchButton(event) {
     event.preventDefault();
     let searchInputElement = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#weather-city");
-    cityElement.innerHTML = searchInputElement.value;
+
     weatherSearchCity(searchInputElement.value);
 
 }
@@ -18,3 +27,5 @@ function searchButton(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit",searchButton);
+
+weatherSearchCity("Paris");
